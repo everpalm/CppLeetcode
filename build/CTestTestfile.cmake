5c5ee674b41b30688841bcdd3f11d4fe8d0c8cd6
Jaron Cheng <everpalm@ms58.url.com.tw>
2025-03-05 18:45:39 +0800

    Chore: Git ignore

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/Jaron/Documents/Projects/CppLeetcode
-# Build directory: /Users/Jaron/Documents/Projects/CppLeetcode/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_suite "/Users/Jaron/Documents/Projects/CppLeetcode/build/test_suite")
-set_tests_properties(test_suite PROPERTIES  _BACKTRACE_TRIPLES "/Users/Jaron/Documents/Projects/CppLeetcode/CMakeLists.txt;30;add_test;/Users/Jaron/Documents/Projects/CppLeetcode/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
