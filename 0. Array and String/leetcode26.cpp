0d57b6ae7b0e99ed256ba8112388ac7f7183d975
Jaron Cheng <everpalm@ms58.url.com.tw>
2025-04-29 13:43:22 +0800

    Refactor: Intuitive

--- 0. Array and String/leetcode26.cpp
+++ 0. Array and String/leetcode26.cpp
@@ -69,15 +69,16 @@ namespace leetcode26 {
         }
         return nums.size();
     }
+    
     int Solution::Optimized(std::vector<int>& nums) {
         if (nums.empty()) return 0;
-        int k = 1; // k 是不重複元素的計數器
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] != nums[k - 1]) {
-                nums[k] = nums[i];
-                k++;
+        int i = 0;
+        for (int j = 1; j < nums.size(); j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
             }
         }
-        return k;
+        return i + 1;
     }
 }
